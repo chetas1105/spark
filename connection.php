197f6a1b4957e6975d31c210cfdbe0335e5820bc
chetas1105 <chetasraulkar774@gmail.com>
2021-05-19 23:36:14 +0530

    added remote  connection  to my data base

--- connection.php
+++ connection.php
@@ -1,9 +1,16 @@
 <!-- connect php to webpage -->
 <?php
- $username ="root";
- $password = "";
- $server = "localhost";
- $db = "sparkb";    
+//devlopment connection
+//  $username ="root";
+//  $password = "";
+//  $server = "localhost";
+//  $db = "sparkb";   
+ 
+//remote database connection
+ $username ="SCLoNaQiYb";
+ $password = "3TZyOOlDp2";
+ $server = "remotemysql.com";
+ $db = "SCLoNaQiYb";  
 
  $con = mysqli_connect($server,$username,$password,$db);
  if($con){
